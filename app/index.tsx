b523b11833c5b2c1c0b29a66457754a570c39824
antoniomattson42 <70714107+antoniomattson42@users.noreply.github.com>
2024-09-09 21:04:35 -0400

    Added buttons for date choice
    added buttons to pick the data type

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,12 @@
-import { Image, Text, View, StyleSheet, Platform } from "react-native";
+import { Image, Text, View, StyleSheet, Platform, Button, Alert } from "react-native";
 import { ThemedText } from '@/components/ThemedText';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { Styles } from "@/constants/Styles";
 import { Stack } from "expo-router";
+import { Dropdown } from "react-native-element-dropdown";
 // api key AIzaSyAlqMcpTEHzNA0ype-TRwH3pmI9_cieJ34
 // weather api key: af491cbdf29213904faf5e4ec7c900c3
+var types = "today"
 export default function index() {
   return (
     <View
@@ -51,8 +53,68 @@ export default function index() {
           flex: 1,
         }}
       >
-        <View>
-          <Text>Hello!</Text>
+          <View style ={{
+            //Box for the text
+            flex: .15,
+            backgroundColor: "",
+            justifyContent:"flex-end",
+            alignContent:"center",
+            marginLeft: 20,
+            marginRight:10,
+          
+          }}>
+            <ThemedText type="subtitle">What type of weather data would you like to search?</ThemedText>
+        </View>
+        <View style ={{
+          flex: .15,
+          justifyContent:"flex-end",
+          alignContent:"center",
+          marginLeft: 20,
+          marginRight:10,
+          flexDirection: "row"
+        }}>
+          <View style={{
+            flex: .33,
+            justifyContent:"center",
+            alignContent:"center",
+            margin:10
+          }}>
+            <Button
+            title="today"
+            onPress={() => {
+              types = "today!",
+              Alert.alert(types)
+            }}
+            />
+          </View>
+          <View style={{
+            flex: .33,
+            justifyContent:"center",
+            alignContent:"center",
+            margin:10
+          }}>
+            <Button
+            title="Next 7 days"
+            onPress={() => {
+              types = "next",
+              Alert.alert(types)
+            }}
+            />
+          </View>
+          <View style={{
+            flex: .33,
+            justifyContent:"center",
+            alignContent:"center",
+            margin:10
+          }}>
+            <Button
+            title="Past 7 Days"
+            onPress={() => {
+              types = "past",
+              Alert.alert(types)
+            }}
+            />
+          </View>
         </View>
       </View>
     </View>
