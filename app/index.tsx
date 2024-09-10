eb5fdec77bf29d7ce2157caf33c75271c3734d2a
antoniomattson42 <70714107+antoniomattson42@users.noreply.github.com>
2024-09-10 19:42:42 -0400

    Revert "Finished API calls"
    This reverts commit 2a9d8319190457ec613cd484d8eaa95a6e9a5874.

--- app/index.tsx
+++ app/index.tsx
@@ -5,11 +5,6 @@ import { Styles } from "@/constants/Styles";
 import TextFields from "@/components/TextField";
 import WeatherScreen from "./WeatherScreen";
 
-
-import { expo as appJson } from '../app.json';
-
-// @ts-ignore
-const packageName: string = appJson.android.package ?? 'host.exp.exponent';
 const Index = ({ navigation }) => {
   const [city, setCity] = useState<string>('');
   const [time, setTime] = useState<string>('Today');
@@ -71,7 +66,7 @@ const Index = ({ navigation }) => {
           marginLeft: 20,
           marginRight: 10,
         }}>
-          <ThemedText type="defaultSemiBold">{packageName}</ThemedText>
+          <ThemedText type="defaultSemiBold">What type of weather data would you like to search?</ThemedText>
         </View>
         <View style={{
           //Buttons
@@ -83,11 +78,10 @@ const Index = ({ navigation }) => {
           flexDirection: "row"
         }}>
           <View style={{
-            flex: 1,
+            flex: .33,
             justifyContent: "center",
             alignContent: "center",
-            margin: 10,
-            marginHorizontal:50,
+            margin: 10
           }}>
             <Button
               title="Today"
@@ -96,7 +90,7 @@ const Index = ({ navigation }) => {
               }}
             />
           </View>
-          {/* <View style={{
+          <View style={{
             flex: .33,
             justifyContent: "center",
             alignContent: "center",
@@ -108,8 +102,8 @@ const Index = ({ navigation }) => {
                 setTime("Tomorrow");
               }}
             />
-          </View> */}
-          {/* <View style={{
+          </View>
+          <View style={{
             flex: .33,
             justifyContent: "center",
             alignContent: "center",
@@ -121,7 +115,7 @@ const Index = ({ navigation }) => {
                 setTime("Yesterday");
               }}
             />
-          </View> */}
+          </View>
         </View>
 
         <View style={{
