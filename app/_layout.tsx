2f2dcfc97728373fdb8980cc3a255ba294e768d7
mielwyn <mielwyn@gmail.com>
2024-09-10 20:38:17 -0400

    fixed issue so app can launch

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,18 +1,11 @@
 import React from 'react';
-import { Tabs } from 'expo-router';
-import { createStackNavigator } from "@react-navigation/stack";
-import Index from './Index';
-import WeatherScreen from './WeatherScreen';
-
-
-const Stack = createStackNavigator();
-
+import { Stack } from 'expo-router';
 
 export default function RootLayout() {
   return (
-      <Stack.Navigator initialRouteName='Index'>
-        <Stack.Screen name="Index" component={Index} options={{headerShown: false}} />
-        <Stack.Screen name="WeatherScreen" component={WeatherScreen} />
-      </Stack.Navigator>
+    <Stack>
+      <Stack.Screen name="index" options={{headerShown: false}} />
+      <Stack.Screen name="WeatherScreen" />
+    </Stack>
   );
 }
